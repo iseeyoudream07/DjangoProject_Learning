080364a0efa21ef2f9516479e1f83de35c2c17fc
HaoHong Zhu <zh15302305858@163.com>
2026-04-06 01:24:56 +0800

    Update settings.py

--- DjangoProject_Learning/settings.py
+++ DjangoProject_Learning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sz%od^10kla2xqhzasav_8-vu_rd_jd66%5vmtns(nfd@kxc=w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iseeyoudream.pythonanywhere.com']
 
 
 # Application definition
@@ -128,3 +129,4 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 LOGIN_URL = 'users:login'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
