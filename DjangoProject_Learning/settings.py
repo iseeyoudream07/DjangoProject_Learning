13641a392298189924d23d94514c71d53e067091
HaoHong Zhu <zh15302305858@163.com>
2026-04-06 21:17:40 +0800

    background supported

--- DjangoProject_Learning/settings.py
+++ DjangoProject_Learning/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sz%od^10kla2xqhzasav_8-vu_rd_jd66%5vmtns(nfd@kxc=w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['iseeyoudream.pythonanywhere.com']
+ALLOWED_HOSTS = ['iseeyoudream.pythonanywhere.com',
+                 '127.0.0.1',
+                 'localhost',]
 
 
 # Application definition
