2d587d553a8dfe7642f2dbb3109d27440a50e341
HaoHong Zhu <zh15302305858@163.com>
2026-04-11 22:42:48 +0800

    更新个人主页并增加默认背景图片

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('', views.index, name='home'),
+    path('profile/', views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from learning_app.models import Topic
 
 # Create your views here.
 def register(request):
@@ -16,3 +18,10 @@ def register(request):
     return render(request, 'users/register.html', context)
 def index(request):
     return render(request, 'learning_app/home.html')
+
+@login_required
+def profile(request):
+    user = request.user
+    topics = Topic.objects.filter(owner=user).order_by('-date_added')
+    context = {'user':user,'topics': topics}
+    return render(request,'users/profile.html',context)
