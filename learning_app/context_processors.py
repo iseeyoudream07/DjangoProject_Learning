0695a252807abdb2a8060d1c6f731f0963d5529d
HaoHong Zhu <zh15302305858@163.com>
2026-04-15 15:03:48 +0800

    美化公告布局

--- learning_app/context_processors.py
+++ learning_app/context_processors.py
@@ -2,4 +2,4 @@ from .models import Announcement
 
 def global_announcement(request):
     announcements = Announcement.objects.filter(is_active=True).order_by('-updated_at').first()
-    return {'site_announcements': announcements}
+    return {'site_announcement': announcements}
